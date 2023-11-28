bd1a2053b5107506ee389f33954647d5f8cf2af6
Dongyeop <ddongyeop0129@gmail.com>
2023-11-29 00:33:12 +0900

    Text print (One by one)

--- 2023_winapi_framework/Text.cpp
+++ 2023_winapi_framework/Text.cpp
@@ -1,14 +1,27 @@
 #include "pch.h"
+#include "define.h"
 #include "Text.h"
 #include "pathMgr.h"
 #include "SelectGDI.h"
+#include "TimeMgr.h"
+#include "KeyMgr.h"
 #include <fstream>
+#include <queue>
 
 Text::Text() 
-	: m_Text{}, m_hFont{}, m_hOldFont{}
+	: m_iPrintIndex(0), m_fPrintTime(0.15f), m_fCurrentTime(0), m_sFrontText{}, m_sPrintText{}
 {
+	m_qPrintQueue = std::queue<wstring>();
 	AddFontResource(TEXT("Galmuri14 Regular.ttf"));
 	AddFontResource(TEXT("Orbit Regular.ttf"));
+}
+
+Text::Text(float _printTime) 
+	: m_iPrintIndex(0), m_fPrintTime(_printTime), m_fCurrentTime(0), m_sFrontText{}, m_sPrintText{}
+{
+	m_qPrintQueue = std::queue<wstring>();
+	AddFontResource(TEXT("Font\\Galmuri14 Regular.ttf"));
+	AddFontResource(TEXT("Font\\Orbit Regular.ttf"));
 }
 
 Text::~Text()
@@ -17,7 +30,20 @@ Text::~Text()
 
 void Text::Update()
 {
-	// update~
+	m_fCurrentTime += fDT;
+
+	if (KeyMgr::GetInst()->GetKey(KEY_TYPE::SPACE) == KEY_STATE::DOWN)
+	{
+		if (m_sPrintText.size() == m_sFrontText.size())
+		{
+			m_sFrontText = m_qPrintQueue.front();
+			m_qPrintQueue.pop();
+		}
+		else
+		{
+			m_sPrintText = m_sFrontText;
+		}
+	}
 }
 
 bool DoesFileExist(const wstring& filePath) 
@@ -28,14 +54,27 @@ bool DoesFileExist(const wstring& filePath)
 
 void Text::Render(HDC _dc)
 {
-	m_hFont = CreateFont(50, 0, 0, 0, 0, 0, 0, 0, HANGEUL_CHARSET, 0, 0, 0, VARIABLE_PITCH | FF_ROMAN, TEXT("Orbit Regular"));
+	if (m_fCurrentTime >= m_fPrintTime)
+	{
+		m_fCurrentTime -= m_fPrintTime;
+		if (m_sFrontText.size() != 0)
+		{
+			if (m_sPrintText.size() < m_sFrontText.size())
+				m_sPrintText += m_sFrontText[m_sPrintText.size()];
+		}
+		else if (!m_qPrintQueue.empty())
+		{
+			m_sFrontText = m_qPrintQueue.front();
+			m_qPrintQueue.pop();
+		}
 
-	m_hOldFont = (HFONT)SelectObject(_dc, m_hFont);
-	//SelectGDI font(_dc, hFont);
+	}
+	HFONT currentFont = CreateFont(50, 0, 0, 0, 0, 0, 0, 0, HANGEUL_CHARSET, 0, 0, 0, VARIABLE_PITCH | FF_ROMAN, TEXT("Orbit Regular"));
+	HFONT oldFont = (HFONT)SelectObject(_dc, currentFont);
 
 	Vec2 pos = GetPos();
 	Vec2 scale = GetScale();
 	RECT rt = RECT_MAKE(pos.x, pos.y, scale.x, scale.y);
-	DrawText(_dc, m_Text.c_str(), m_Text.length(), &rt, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
-	SelectObject(_dc, m_hOldFont);
+	DrawText(_dc, m_sPrintText.c_str(), m_sPrintText.length(), &rt, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+	SelectObject(_dc, oldFont);
 }
